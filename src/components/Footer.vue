2340ecad45b834ddbd758ab687928e8eb0817080
Aakeefah Jones <aakeefahj@gmail.com>
2024-02-26 16:25:46 +0200

    footer changed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="container">
+    <div class="col 18 m24 s12 row links">
+      <div class="col 18 m24 s12">
+        <h2 class="h6" js-footer="toggle">About Us:</h2>
+        <ul js-footer="menu">
+          <li>
+            <a href="">Meet the team</a>
+          </li>
+          <li>
+            <a href="">Benefits</a>
+          </li>
+          <li>
+            <a href="">Where we are located</a>
+          </li>
+          <li>
+            <a href="/about">Our Careers</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col 18 m24 s12">
+        <h2 class="h6" js-footer="toggle">Aakeefah</h2>
+        <ul js-footer="menu">
+          <li>
+            <i class="bi bi-whatsapp"> +27 79 866 6734</i>
+          </li>
+          <li>
+            <i class="bi bi-instagram"><a href="https://www.instagram.com/_aakeefahjones_/" target="_blank"> _aakeefahjones_</a></i><br>
+          </li>
+          <li>
+            <i class="bi bi-github"><a href="https://github.com/keeefles" target="_blank"> @keeefles</a></i><br>
+          </li>
+        </ul>
+      </div>
+      <div class="col 18 m24 s12">
+        <h2 class="h6" js-footer="toggle">Veronique</h2>
+        <ul js-footer="menu">
+          <li>
+            <i class="bi bi-whatsapp"> +27 65 944 7142</i>
+          </li>
+          <li>
+            <i class="bi bi-instagram"><a href="https://www.instagram.com/_vdrossouw" target="_blank"> _vdrossouw</a></i>
+          </li>
+          <li>
+            <i class="bi bi-github"><a href="https://github.com/veroniquex1" target="_blank"> @veroniquex1</a></i>
+          </li>
+        </ul>
+      </div>
+    </div>
       <div class="row">
           <p class="footer">BLUE<span class="femme">femme</span> &copy; <span>{{ currYear }}</span></p>
       </div>
@@ -18,7 +65,20 @@
 </script>
 
 <style scoped>
-
+li {
+  text-decoration: none;
+  display: list-item;
+  text-align: -webkit-match-parent;
+}
+ul {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}.container {
+  margin-top: 20px;
+}
 .footer {
   padding-top: 10px;
   bottom: 0;
