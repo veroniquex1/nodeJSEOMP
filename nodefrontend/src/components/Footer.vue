b999b48bd65e73d24dca7f6c6b9ac1753856631f
Aakeefah Jones <aakeefahj@gmail.com>
2024-02-26 15:03:50 +0200

    navbar and footer added

---
+++ nodefrontend/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <p class="footer">BLUE<span class="femme">femme</span> &copy; <span>{{ currYear }}</span></p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComp',
+        data() {
+            return {
+                currYear: new Date().getFullYear()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.footer {
+    padding-top: 10px;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+}
+
+</style>
