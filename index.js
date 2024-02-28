2f01ae922921700e8ccda63876a051e347d1935b
veroniquex1 <vdrossouw@gmail.com>
2024-02-28 10:43:29 +0200

    main index content added

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("hello world");
+import { userRouter, express } from './controller/UserController.js'
+import { productRouter } from './controller/ProductController.js';
+import { errorHandling } from './middleware/errorHandling.js'
+import cookieParser from 'cookie-parser';
+import path from 'path'
+import cors from 'cors'
+import { config } from "dotenv";
+config()
+
+const app = express();
+// app.use(express.json());
+
+const port = +process.env.port || 4000
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorisation");
+    next();
+})
+
+app.use(
+    express.static('./static'),
+    express.json(),
+    express.urlencoded({
+        extended: true,
+    }),
+    cookieParser(),
+    cors()
+)
+app.get('^/$|/blueFemmeDB', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './static/index.html'))
+})
+app.use('/users', userRouter)
+app.use('/products', productRouter)
+app.use(errorHandling)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
